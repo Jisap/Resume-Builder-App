29c93b75001a9810f75dccafd396395ccd2caec3
Jorge Matos <jisapjisap@gmail.com>
2026-07-03 19:03:38 +0200

    feat: implement dashboard page to view, create, upload, and manage user resumes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -36,6 +36,13 @@ const Dashboard = () => {
     event.preventDefault();
   }
 
+  const deleteResume = async (resumeId) => {
+    const confirm = window.confirm("Are you sure to delete this resume?");
+    if (!confirm) {
+      setAllResumes(prev = prev.filter(resume => resume._id !== resumeId))
+    }
+  }
+
   useEffect(() => {
     loadAllResumes();
   }, [])
@@ -99,7 +106,10 @@ const Dashboard = () => {
                 <div
                   onClick={(e) => e.stopPropagation()}
                   className='absolute top-1 right-1 group-hover:flex items-center hidden'>
-                  <TrashIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors' />
+                  <TrashIcon
+                    onClick={() => deleteResume(resume._id)}
+                    className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'
+                  />
                   <PencilIcon
                     onClick={() => {
                       setEditResumeId(resume._id)
