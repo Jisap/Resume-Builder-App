85a669d93ada5274cf11cf7fd6553b0c66d32c95
Jorge Matos <jisapjisap@gmail.com>
2026-07-03 18:33:40 +0200

    feat: implement dashboard page and global CSS styles for resume management

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloudIcon, XIcon } from 'lucide-react'
+import { FilePenLineIcon, PencilIcon, PlusIcon, TrashIcon, UploadCloud, UploadCloudIcon, XIcon } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import { dummyResumeData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
@@ -24,7 +24,12 @@ const Dashboard = () => {
     event.preventDefault();
     setShowCreateResume(false);
     navigate(`/app/builder/res123`)
+  }
 
+  const uploadResume = async (event) => {
+    event.preventDefault();
+    setShowUploadResume(false);
+    navigate(`/app/builder/res123`)
   }
 
   useEffect(() => {
@@ -47,7 +52,10 @@ const Dashboard = () => {
             <p className='text-sm group-hover:text-indigo-600 transition-all duration-300'>Create Resume</p>
           </button>
 
-          <button className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
+          <button
+            onClick={() => setShowUploadResume(true)}
+            className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer'
+          >
             <UploadCloudIcon className='size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from purple-300 to-purple-500 text-white rounded-full' />
             <p className='text-sm group-hover:text-purple-600 transition-all duration-300'>Upload Existing</p>
           </button>
@@ -120,6 +128,62 @@ const Dashboard = () => {
                   className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors'
                   onClick={() => {
                     setShowCreateResume(false)
+                    setTitle("")
+                  }}
+                />
+              </div>
+            </form>
+          )
+        }
+
+        {
+          showUploadResume && (
+            <form
+              onSubmit={uploadResume}
+              onClick={() => setShowUploadResume(false)}
+              className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'
+            >
+              <div
+                onClick={e => e.stopPropagation()}
+                className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6'
+              >
+                <h2>Upload a Resume</h2>
+
+                <input
+                  type="text"
+                  placeholder='Enter resume title'
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                  className='w-full px-4 py-2 mb-4 focus:border-green-600 ring-green-600'
+                  required
+                />
+
+                <div>
+                  <label htmlFor="resume-input" className='block text-sm text-slate-700'>
+                    Select resume file
+                    <div className='flex flex-col items-center justify-center gap-2 border group text-slate-400 border-slate-400
+                     border-dashed rounded-md p-4 py-10 my-4 hover:border-green-500 hover:text-green-700 cursor-pointer transition-colors'
+                    >
+                      {resume ? (
+                        <p className='text-green-700'>{resume.name}</p>
+                      ) : (
+                        <>
+                          <UploadCloud className="size-14 stroke-1" />
+                          <p>Upload Resume</p>
+                        </>
+                      )}
+                    </div>
+                  </label>
+                </div>
+
+                <button className='w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 transition-colors'>
+                  Upload Resume
+                </button>
+
+                <XIcon
+                  className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors'
+                  onClick={() => {
+                    setShowUploadResume(false)
                     setTitle("")
                   }}
                 />
