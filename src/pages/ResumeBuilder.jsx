a3285011235be9ca24343017dbccd0c7246d62c8
Jorge Matos <jisapjisap@gmail.com>
2026-07-04 12:07:36 +0200

    feat: create ResumeBuilder page component with multi-section navigation structure

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom';
+import { dummyResumeData } from '../assets/assets';
+import { ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react';
 
 const ResumeBuilder = () => {
-    return (
-        <div>ResumeBuilder</div>
-    )
+
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  });
+
+  const loadExistingResume = () => {
+    const resume = dummyResumeData.find(resume => resume._id === resumeId)
+    if (resume) {
+      setResumeData(resume)
+      document.title = resume.title  // Cambio de nomber de la pestaña del navegador
+    }
+  };
+
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+
+  const sections = [
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "Summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ]
+
+  const activeSection = sections[activeSectionIndex]
+
+  useEffect(() => {
+    loadExistingResume()
+  }, [resumeId])
+
+  return (
+    <div className='max-w-7xl mx-auto px-4 py-6'>
+      <div>
+        <Link
+          to={"/app"}
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all"
+        >
+          <ArrowLeftIcon className='size-4' /> Back to Dashboard
+        </Link>
+      </div>
+
+      <div className='max-w-7xl mx-auto px-4 pb-8'>
+        <div className='grid lg:grid-cols-12 gap-8'>
+          {/* Left Panel - Form */}
+          <div className='relative lg:cols-span-5 rounded-lg overflow-hidden'>
+            <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
+              {/* Progress bar using activeSectionIndex */}
+              <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200'></hr>
+              <hr className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-200'
+                style={{ width: `${activeSectionIndex * 100 / (sections.length - 1)}%` }}
+              ></hr>
+            </div>
+          </div>
+
+          {/* Right Panel - Preview */}
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default ResumeBuilder
