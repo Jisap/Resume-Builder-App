d6cb7d037ccf40d921a0c87a1c0e1aa75d1f94c5
Jorge Matos <jisapjisap@gmail.com>
2026-07-09 16:03:41 +0200

    feat: implement application routing, API configuration, and authentication state initialization

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,37 @@ import Dashboard from "./pages/Dashboard"
 import ResumeBuilder from "./pages/ResumeBuilder"
 import Preview from "./pages/Preview"
 import Login from "./pages/Login"
+import { useDispatch } from "react-redux"
+import api from "./configs/api"
+import { login, setLoading } from "./app/features/authSlice"
+import { useEffect } from "react"
 
 
 const App = () => {
+
+  const dispatch = useDispatch();                      // Función que permite enviar acciones a Redux
+  const getUserData = async () => {
+    const token = localStorage.getItem('token');       // Obtenemos el token del localStorage
+    try {
+      if (token) {
+        const { data } = await api.get("/api/user/data", { headers: { Authorization: token } })  // Petición GET al backend para obtener los datos del usuario
+        if (data.user) {                                                                         // Si obtenemos los datos del usuario, 
+          dispatch(login({ token, user: data.user }))                                            // los enviamos a Redux
+        }
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    } catch (error) {
+      console.error(error.message);
+      dispatch(setLoading(false));
+    }
+  }
+
+  useEffect(() => {
+    getUserData()  // Llamamos a la función para obtener los datos del usuario al montar el componente
+  }, [])
+
   return (
     <>
       <Routes>
